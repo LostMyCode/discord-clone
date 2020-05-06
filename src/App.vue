096754cec0f78160aaa75bb9460f200e72e6c4aa
test114514 <bakamono.cx@gmail.com>
2020-05-06 21:58:25 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <router-view />
+    <WelcomeOverlay />
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WelcomeOverlay from "./components/WelcomeOverlay";
+
+//console.say = console.log;
+console.say = function(){}
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    WelcomeOverlay,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./main.scss";
+@import "./style.scss";
 </style>
